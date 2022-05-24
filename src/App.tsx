788fee172ee10a64117539300eda22aaefe6b30c
Nithya Thayananthan <nithyathaya@gmail.com>
2022-05-24 15:02:41 -0400

    context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import LanguageContext from './context/LanguageContext';
+import ColorContext from './context/ColorContext';
 import Home from './pages/Home';
 import News from './pages/News';
 import About from './pages/About';
@@ -8,16 +11,26 @@ import Footer from './pages/Footer';
 import Navigation from './pages/Navigation';
 
 const App = () => {
+  const [lang, setLang] = useState('english');
   return (
     <div>
-      <Navigation />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='news/:newid' element={<News />} />
-        <Route path='about' element={<About />} />
-        <Route path='grocery' element={<GroceryApp />} />
-      </Routes>
-      <Footer />
+      <LanguageContext.Provider value={lang}>
+        <ColorContext.Provider value='blue'>
+          <Navigation />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='news/:newid' element={<News />} />
+            <Route path='about' element={<About />} />
+            <Route path='grocery' element={<GroceryApp />} />
+          </Routes>
+          <Footer />
+        </ColorContext.Provider>
+      </LanguageContext.Provider>
+      <div className='App'>
+        React Context Text {lang}
+        <button onClick={() => setLang('English')}>English</button>
+        <button onClick={() => setLang('French')}>French</button>
+      </div>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ColorContext from '../context/ColorContext';
+import LanguageContext from '../context/LanguageContext';
 
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div className='page-style'>Home</div>;
+  const lang = useContext(LanguageContext);
+  const col = useContext(ColorContext);
+  return (
+    <div className='page-style'>
+      Home {col} {lang}
+    </div>
+  );
 };
 
 export default Home;
