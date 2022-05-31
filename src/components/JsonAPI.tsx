b59a7b6d4dfff1e9b8372521be798a34fc138591
Nithya Thayananthan <nithyathaya@gmail.com>
2022-05-31 14:36:28 -0400

    crud post

--- src/components/JsonAPI.tsx
+++ src/components/JsonAPI.tsx
@@ -13,13 +13,63 @@ const JsonAPI = () => {
   const [posts, setPosts] = useState<PostType[]>([]);
 
   const sendGetRequest = async () => {
-    const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    );
-    setPosts(response.data);
-    console.log(response);
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      setPosts(response.data);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
+  const sendDeleteRequest = async () => {
+    try {
+      const response = await axios.delete(
+        'https://jsonplaceholder.typicode.com/posts/1'
+      );
+
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const sendPostRequest = async () => {
+    try {
+      const response = await axios.post(
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+          title: 'foo',
+          body: 'bar',
+          userId: 1,
+        }
+      );
+
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const sendPutRequest = async () => {
+    try {
+      const response = await axios.put(
+        'https://jsonplaceholder.typicode.com/posts/1',
+        {
+          id: 1,
+          title: 'foo',
+          body: 'bar',
+          userId: 1,
+        }
+      );
+
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   useEffect(() => {
     // fetch('https://jsonplaceholder.typicode.com/posts')
     //   .then((response) => response.json())
@@ -39,6 +89,9 @@ const JsonAPI = () => {
 
   return (
     <div>
+      <button onClick={sendPostRequest}>Add Post</button>
+      <button onClick={sendPutRequest}>Update Post</button>
+      <button onClick={sendDeleteRequest}>Delete Post</button>
       {posts.map((post) => (
         <Post post={post} key={post.id} />
       ))}
